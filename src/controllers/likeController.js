46ae9e44c228879d125d49f08ab1f79af710f143
leodaiub <leodaiub@gmail.com>
2019-08-16 23:42:31 -0300

    adding websocket

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -1,19 +1,30 @@
-const dev = require('../models/dev');
+const Dev = require('../models/Dev');
 
 module.exports = {
-    async store( req, res) {
+    async store(req, res) {
+        console.log(req.io, req.connectedUsers);
+
         const { user } = req.headers;
         const { devId } = req.params;
 
-        const loggedDev = await dev.findById(user);
-        const targetDev = await dev.findById(devId);
+        const loggedDev = await Dev.findById(user);
+        const targetDev = await Dev.findById(devId);
 
         if (!targetDev) {
-            return res.status(400).json('dev not exists');
+            return res.status(400).json({ error: 'Dev not exists' });
         }
 
         if (targetDev.likes.includes(loggedDev._id)) {
-            console.log('deu match');
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+            
+            if (loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         }
 
         loggedDev.likes.push(targetDev._id);
